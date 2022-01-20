4fba5c703771524764f494ad5b9df07c4e2ec838
Thiên Sứ <91358817+thiensu99@users.noreply.github.com>
2022-01-20 13:53:14 +0000

    Add files via upload

---
+++ TS_Theme/dashboard.blade.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/theme/bob-argon/favicon.ico">
+    <title>4G GIÁ RẺ</title>
+    <link rel="stylesheet" href="https://unpkg.com/element-plus/lib/theme-chalk/index.css">
+    <link href="theme/Bob-Theme-Argon/css/app.27b31f9d.css" rel="preload" as="style">
+    <link href="theme/Bob-Theme-Argon/css/chunk-vendors.9f69bc1a.css" rel="preload" as="style">
+    <link href="theme/Bob-Theme-Argon/js/app.b5eb06b2.js" rel="preload" as="script">
+    <link href="theme/Bob-Theme-Argon/js/chunk-vendors.46c7013e.js" rel="preload" as="script">
+    <link href="theme/Bob-Theme-Argon/css/chunk-vendors.9f69bc1a.css" rel="stylesheet">
+    <link href="theme/Bob-Theme-Argon/css/app.27b31f9d.css" rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+<script>
+    window.APP_DESCRIPTION = '{{$description}}';
+    window.APP_NAME = '{{$title}}';
+    const SCRIPT_ID = '{{$crisp_id}}'; // 填写Crisp_id即可开启crisp客服
+    if (SCRIPT_ID) {
+        window.$crisp = [];
+        window.CRISP_WEBSITE_ID = SCRIPT_ID;
+        (function () {
+            d = document;
+            s = d.createElement("script");
+            s.src = "https://client.crisp.chat/l.js";
+            s.async = 1;
+            d.getElementsByTagName("head")[0].appendChild(s);
+        })();
+    }</script>
+<script src="theme/Bob-Theme-Argon/js/chunk-vendors.46c7013e.js"></script>
+<script src="theme/Bob-Theme-Argon/js/app.b5eb06b2.js"></script>
+<!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "100370419221178");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+</body>
+</html>
